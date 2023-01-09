e47569b836599ec789fe497a2db4ff5333d4bcf2
OxSama <mhmdtageldin@gmail.com>
2023-01-09 11:03:45 +0200

    edit in the base layout'

--- src/layouts/baseLayout.vue
+++ src/layouts/baseLayout.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="bg-[url('../assets/imgs/sfb-bg.jpg')] grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 max-sm:grid-cols-1 bg-cover max-sm:bg-primary">
+  <div class="lg:bg-[url('../assets/imgs/sfb-bg.jpg')] grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 max-sm:grid-cols-1 bg-cover max-sm:bg-primary md:bg-primary h-full">
     <!-- <img src="./assests/imgs/sfb-logo.png" alt="" /> -->
-    <div class="columns-1">
+    <div class="lg:columns-1 max-sm:h-full max-sm:w-full">
       <div class="backdrop-blur-xl rounded-xl lg:w-5/6 sm:w-full">
         <div class="flex lg:flex-row max-sm:flex-col md:flex-col lg:flex-nowrap md:flex-wrap sm:flex-wrap break-words items-center justify-center mt-10 mb-2 p-2 w-full">
           <img class="w-72 mr-2" src="../assets/imgs/SFB-logo.png" alt="" />
-            <h1 class="text-3xl max-sm:text-white text-black font-serif">Online Account Opening</h1>
+            <h1 class="text-3xl text-white drop-shadow-2xl font-serif">Online Account Opening</h1>
         </div>
       </div>
     </div>
     <!-- <router-view /> -->
-    <slot />
+    <div class="flex lg:items-center justify-center lg:mr-10 sm:m-0 lg:h-screen h-full columns-2">
+
+        <slot />
+        
+    </div>
   </div>
 </template>
