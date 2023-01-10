d9702e0263f5345ec5f2f2ca5108c1f9e649e2f1
OxSama <mhmdtageldin@gmail.com>
2023-01-10 23:03:45 +0200

    edit the import of an image

--- src/components/TheBg.vue
+++ src/components/TheBg.vue
@@ -2,7 +2,7 @@
   <div class="bg-primary h-screen">
     <div class="flex justify-center sm:items-center md:items-center max-sm:items-center">
       <div class="flex items-center justify-center mt-2 object-cover max-sm:px-6 sm:px-6">
-        <img class="mr-2" src="../assets/imgs/SFB-logo-2.png" alt="" />
+        <img class="mr-2" src="@/assets/imgs/SFB-logo-2.png" alt="" />
         <h1 class="ml-2 text-3xl max-sm:text-xl sm:text-xl text-white font-serif">
           Online Account Opening
         </h1>
