10e79aaa43d4c2a640efd3a4a324a5056a29b3c0
OxSama <mhmdtageldin@gmail.com>
2023-01-10 22:59:22 +0200

    moved the form to the layout folder

--- src/layouts/Form.vue
+++ src/layouts/Form.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import FormCard from "../components/FormCard.vue";
-import TheBg from "../components/TheBg.vue";
+import FormCard from "@/components/FormCard.vue";
+import TheBg from "@/components/TheBg.vue";
 </script>
 <template>
   <TheBg>
