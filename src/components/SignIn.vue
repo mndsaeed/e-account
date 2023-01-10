ec0f80ecce2df47467a29b488722ed230fc40fcc
OxSama <mhmdtageldin@gmail.com>
2023-01-10 12:44:05 +0200

    bug in the router in sign in view directs to /home

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -51,7 +51,7 @@
             >
           </router-link>
         </div>
-        <router-link to="/home/signedin">
+        <router-link to="/signedin">
           <button
             type="submit"
             class="w-full mt-2 text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -55,7 +55,7 @@ import baseLayout from '../layouts/baseLayout.vue';
                     >
                   </router-link>
                 </div>
-                <router-link to="/home/signedin">
+                <router-link to="signedin">
                   <button
                     type="submit"
                     class="w-full mt-2 text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
